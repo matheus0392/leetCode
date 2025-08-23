2e8ae135f06bbe74999c06ee8abb0a08fa10c009
unknown <mnascimento0392@gmail.com>
2025-08-22 20:48:02 -0400

    8. String to Integer (atoi)

---
+++ src/main/java/dev/mnascimento0392/leetCode/_8/StringToInteger.java
@@ -0,0 +1,7 @@
+package dev.mnascimento0392.leetCode._8;
+
+class Solution {
+	public int myAtoi(String s) {
+		return 0;
+	}
+}
---
+++ src/test/java/dev/mnascimento0392/leetCode/_8/StringToIntegerTest.java
@@ -0,0 +1,46 @@
+package dev.mnascimento0392.leetCode._8;
+
+import static dev.mnascimento0392.leetCode.TestFormatter.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import lombok.extern.java.Log;
+
+@Log
+@TestInstance(Lifecycle.PER_CLASS)
+class StringToIntegerTest {
+
+	@BeforeAll
+	void test() {
+		System.err.println("8. String to Integer (atoi)");
+	}
+
+	@Test
+	void test1() {
+		assertFormatted(new Solution().myAtoi("42"), 42);
+	}
+
+	@Test
+	void test2() {
+		assertFormatted(new Solution().myAtoi(" -042"), -42);
+	}
+
+	@Test
+	void test3() {
+		assertFormatted(new Solution().myAtoi("1337c0d3"), 1337);
+	}
+
+	@Test
+	void test4() {
+		assertFormatted(new Solution().myAtoi("0-1"), 0);
+	}
+
+	@Test
+	void test5() {
+		assertFormatted(new Solution().myAtoi("words and 987"), 0);
+	}
+
+}
