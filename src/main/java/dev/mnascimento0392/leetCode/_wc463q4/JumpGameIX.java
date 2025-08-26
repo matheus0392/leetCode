1a9e34e9fc67b2a8f80acbcbf700f088da657d69
unknown <mnascimento0392@gmail.com>
2025-08-25 23:44:27 -0400

    Q3. Jump Game IX

--- src/main/java/dev/mnascimento0392/leetCode/_wc463q4/JumpGameIX.java
+++ src/main/java/dev/mnascimento0392/leetCode/_wc463q4/JumpGameIX.java
@@ -1,11 +1,128 @@
 package dev.mnascimento0392.leetCode._wc463q4;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 class Solution {
-	public int[] maxValue(int[] nums) {
-		return nums;
 
+	public int[] maxValue(int[] nums) {
+		int ans[];
+
+		ans = new int[nums.length];
+
+		for (int i = 0; i < nums.length; i++) {
+			int k = nums[i];
+
+			for (int x = 0; x < nums.length; x++) {
+				if (x < i) {
+					if (nums[x] > k) {
+						k = nums[x];
+					}
+				} else if (x > i) {
+					if (nums[x] > k) {
+						for (int y = x; y < nums.length; y++) {
+							if (nums[y] < k) {
+								k = nums[x];
+								break;
+							}
+						}
+					}
+				}
+			}
+			ans[i] = k;
+		}
+
+		return ans;
 	}
+
+	/*public int[] maxValue2(int[] nums) {
+
+		ans = new int[nums.length];
+		Arrays.fill(ans, -1);
+
+		for (int i = 0; i < nums.length; i++) {
+			boolean found = false;
+
+			for (int j = i; j >= 0; j--) {
+				if (history.get(j) != null && history.get(j).contains(i)) {
+					ans[i] = ans[j];
+					Set<Integer> newSet = history.get(j);
+					newSet.add(i);
+					history.put(j, newSet);
+					found = true;
+					break;
+				}
+			}
+
+			if (!found) {
+//----------------------------------------------
+				// ans[i] = jumpGame(nums, i);
+//----------------------------------------------
+				int k = nums[i];
+				set.add(i);
+
+				for (int x = 0; x < nums.length; x++) {
+					if (x < i) {
+						if (nums[x] > k) {
+							k = nums[x];
+							set.add(x);
+						}
+					} else if (x > i) {
+						if (nums[x] > k) {
+							for (int y = x; y < nums.length; y++) {
+								if (nums[y] < k) {
+									k = nums[x];
+									set.add(y);
+									set.add(x);
+									break;
+								}
+							}
+						}
+					}
+				}
+				ans[i] = k;
+//----------------------------------------------
+				history.put(i, set);
+				set = new HashSet<Integer>();
+			}
+		}
+
+		return ans;
+	}
+
+	private int jumpGame(int[] nums, int i) {
+
+		set.add(i);
+
+		int val = nums[i];
+
+		for (int j = i - 1; j >= 0; j--) {
+			if (nums[i] < nums[j]) {
+				if (ans[j] > -1) {
+					return ans[j];
+				}
+				if (!set.contains(j)) {
+					int val2 = jumpGame(nums, j);
+					val = val2 > val ? val2 : val;
+				}
+			}
+		}
+
+		for (int j = i + 1; j < nums.length; j++) {
+			if (nums[i] > nums[j]) {
+				if (!set.contains(j)) {
+					int val2 = jumpGame(nums, j);
+					val = val2 > val ? val2 : val;
+				}
+				if (j >= nums.length) {
+					return val;
+				}
+			}
+		}
+
+		return val;
+	}*/
 }
--- src/test/java/dev/mnascimento0392/leetCode/_wc463q4/JumpGameIXTest.java
+++ src/test/java/dev/mnascimento0392/leetCode/_wc463q4/JumpGameIXTest.java
@@ -1,31 +1,58 @@
 package dev.mnascimento0392.leetCode._wc463q4;
 
-import static dev.mnascimento0392.leetCode.TestFormatter.*;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+import dev.mnascimento0392.leetCode.TestFormatter;
 import lombok.extern.java.Log;
 
 @Log
 @TestInstance(Lifecycle.PER_CLASS)
-class JumpGameIXTest {
+class JumpGameIXTest extends TestFormatter {
 
-	@BeforeAll
-	void test() {
-		System.err.println("Q2. Partition Array Into K-Distinct Groups");
+	JumpGameIXTest() {
+		setQuestion("Q3. Jump Game IX");
+	}
+
+	@Test
+	void test0() {
+		assertArrayFormatted(new Solution().maxValue(new int[] { 3, 2, 1 }), new int[] { 3, 3, 3 });
 	}
 
 	@Test
 	void test1() {
-		assertFormatted(new Solution().maxValue(new int[] { 1, 2, 3, 4 }), new int[] { 1, 2, 3, 4 });
+		assertArrayFormatted(new Solution().maxValue(new int[] { 2, 1, 3 }), new int[] { 2, 2, 3 });
 	}
 
 	@Test
 	void test2() {
-		assertFormatted(new Solution().maxValue(new int[] { 3, 5, 2, 2 }), new int[] { 1, 2, 3, 4 });
+		assertArrayFormatted(new Solution().maxValue(new int[] { 2, 3, 1 }), new int[] { 3, 3, 3 });
 	}
 
+	@Test
+	void test4() {
+		assertArrayFormatted(new Solution().maxValue(new int[] { 8, 12 }), new int[] { 8, 12 });
+	}
+
+	@Test
+	void test5() {
+		assertArrayFormatted(new Solution().maxValue(new int[] { 1 }), new int[] { 1 });
+	}
+
+	@Test
+	void test6() {
+		assertArrayFormatted(new Solution().maxValue(new int[] { 2, 3, 1, 4 }), new int[] { 3, 3, 3, 4 });
+	}
+
+	@Test
+	void test7() {
+		assertArrayFormatted(new Solution().maxValue(new int[] { 14, 9, 3, 10 }), new int[] { 14, 14, 14, 14 });
+	}
+
+	@Test
+	void test8() {
+		assertArrayFormatted(new Solution().maxValue(new int[] { 30, 21, 5, 35, 24 }),
+				new int[] { 35, 35, 35, 35, 35 });
+	}
 }
