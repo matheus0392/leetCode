d6ca6e3598429fc76bfd080c7e280f491713fbcc
unknown <mnascimento0392@gmail.com>
2025-09-05 22:42:06 -0400

    18. 4Sum

--- src/main/java/dev/mnascimento0392/leetCode/_18/FourSum.java
+++ src/main/java/dev/mnascimento0392/leetCode/_18/FourSum.java
@@ -1,10 +1,42 @@
 package dev.mnascimento0392.leetCode._18;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-		return null;
-        
-    }
+	public List<List<Integer>> fourSum(int[] nums, int target) {
+
+		List<List<Integer>> result = new ArrayList<>();
+
+		Arrays.sort(nums);
+
+		for (int i = 0; i < 2; i++) {
+			for (int x = 0; x < nums.length - 1; x++) {
+				for (int y = nums.length - 1; y > 0 && y > x - 1; y--) {
+					for (int w = x + 1, z = y - 1; z > w;) {
+						if (target == (long) nums[x] + nums[w] + nums[z] + nums[y]) {
+
+							if (result.contains(List.of(nums[x], nums[w], nums[z], nums[y]))) {
+								if (i % 2 == 0) {
+									w++;
+								} else {
+									z--;
+								}
+								continue;
+							}
+							result.add(List.of(nums[x], nums[w], nums[z], nums[y]));
+						}
+						if (nums[x] + nums[w] + nums[z] + nums[y] < target) {
+							w++;
+						} else {
+							z--;
+						}
+					}
+				}
+			}
+		}
+
+		return result;
+	}
 }
--- src/test/java/dev/mnascimento0392/leetCode/_18/FourSumTest.java
+++ src/test/java/dev/mnascimento0392/leetCode/_18/FourSumTest.java
@@ -28,4 +28,31 @@ class FourSumTest extends TestFormatter {
 		assertFormatted(new Solution().fourSum(new int[] { 2, 2, 2, 2, 2 }, 8), List.of(List.of(2, 2, 2, 2)));
 	}
 
+	@Test
+	void test3() {
+		assertFormatted(new Solution().fourSum(new int[] { -3, -2, -1, 0, 0, 1, 2, 3 }, 0),
+				List.of(List.of(-3, -2, 2, 3), List.of(-3, -1, 1, 3), List.of(-3, 0, 0, 3), List.of(-2, -1, 0, 3),
+						List.of(-2, -1, 1, 2), List.of(-2, 0, 0, 2), List.of(-1, 0, 0, 1), List.of(-3, 0, 1, 2)));
+	}
+
+	@Test
+	void test4() {
+		assertFormatted(new Solution().fourSum(new int[] { -5, -4, -3, -2, -1, 0, 0, 1, 2, 3, 4, 5 }, 0),
+				List.of(List.of(-5, -4, 4, 5), List.of(-5, -3, 3, 5), List.of(-5, -2, 2, 5), List.of(-5, -1, 1, 5),
+						List.of(-5, 0, 0, 5), List.of(-5, -2, 3, 4), List.of(-5, -1, 2, 4), List.of(-5, 0, 1, 4),
+						List.of(-5, 0, 2, 3), List.of(-4, -3, 2, 5), List.of(-4, -2, 1, 5), List.of(-4, -1, 0, 5),
+						List.of(-4, -3, 3, 4), List.of(-4, -2, 2, 4), List.of(-4, -1, 1, 4), List.of(-4, 0, 0, 4),
+						List.of(-4, -1, 2, 3), List.of(-4, 0, 1, 3), List.of(-3, -2, 0, 5), List.of(-3, -2, 1, 4),
+						List.of(-3, -1, 0, 4), List.of(-3, -2, 2, 3), List.of(-3, -1, 1, 3), List.of(-3, 0, 0, 3),
+						List.of(-3, 0, 1, 2), List.of(-2, -1, 0, 3), List.of(-2, -1, 1, 2), List.of(-2, 0, 0, 2),
+						List.of(-1, 0, 0, 1)));
+	}
+
+	@Test
+	void test0() {
+		assertFormatted(
+				new Solution().fourSum(new int[] { 1000000000, 1000000000, 1000000000, 1000000000 }, -294967296),
+				List.of());
+	}
+
 }
