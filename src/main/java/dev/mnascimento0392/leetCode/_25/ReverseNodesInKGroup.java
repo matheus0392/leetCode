c1b64d8a8f1b8243392e4a6432219310ec29b89b
unknown <mnascimento0392@gmail.com>
2025-09-14 21:03:26 -0400

    25. Reverse Nodes in k-Group

--- src/main/java/dev/mnascimento0392/leetCode/_25/ReverseNodesInKGroup.java
+++ src/main/java/dev/mnascimento0392/leetCode/_25/ReverseNodesInKGroup.java
@@ -1,20 +1,68 @@
 package dev.mnascimento0392.leetCode._25;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import dev.mnascimento0392.leetCode.Definitions.ListNode;
 
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
  */
 class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-		return head;
-        
-    }
+	public ListNode reverseKGroup(ListNode head, int k) {
+
+		int count = 0;
+		ListNode aux = head;
+		while (aux != null) {
+			count++;
+			aux = aux.next;
+		}
+
+		if (count < k || k == 1) {
+			return head;
+		}
+
+		ListNode resp = new ListNode(0, head);
+		resp.next = reverseNextK(head, count, k);
+		return resp.next;
+	}
+
+	private ListNode reverseNextK(ListNode head, int count, int k) {
+		if (count < k) {
+			return head;
+		}
+
+		ListNode reverted = ReverseK(head, k);
+		ListNode aux = reverted;
+		while (aux.next != null) {
+			aux = aux.next;
+			head = head.next;
+		}
+		aux.next = reverseNextK(head.next, count - k, k);
+		return reverted;
+	}
+
+	private ListNode ReverseK(ListNode head, int k) {
+
+		List<Integer> rev = new ArrayList<>();
+		ListNode aux = head;
+		for (int i = 0; i < k; i++) {
+			rev.add(aux.val);
+			aux = aux.next;
+		}
+
+		aux = new ListNode(0);
+		ListNode aux2 = aux;
+		for (int i = rev.size() - 1; i > 0; i--) {
+			aux.val = rev.get(i);
+			aux.next = new ListNode(0);
+			aux = aux.next;
+		}
+
+		aux.val = rev.get(0);
+
+		return aux2;
+	}
 }
--- src/test/java/dev/mnascimento0392/leetCode/_25/ReverseNodesInKGroupTest.java
+++ src/test/java/dev/mnascimento0392/leetCode/_25/ReverseNodesInKGroupTest.java
@@ -26,4 +26,22 @@ class ReverseNodesInKGroupTest extends TestFormatter {
 		assertFormatted(new Solution().reverseKGroup(new ListNode(1, 2, 3, 4, 5), 3), new ListNode(3, 2, 1, 4, 5));
 	}
 
+	@Test
+	void test3() {
+		assertFormatted(new Solution().reverseKGroup(new ListNode(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), 3),
+				new ListNode(3, 2, 1, 6, 5, 4, 9, 8, 7, 12, 11, 10, 13, 14));
+	}
+
+	@Test
+	void test4() {
+		assertFormatted(new Solution().reverseKGroup(new ListNode(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), 1),
+				new ListNode(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14));
+	}
+	
+	@Test
+	void test5() {
+		assertFormatted(new Solution().reverseKGroup(new ListNode(1), 4),
+				new ListNode(1));
+	}
+
 }
