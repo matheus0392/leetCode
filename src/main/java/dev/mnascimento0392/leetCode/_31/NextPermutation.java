6a89628b187645c9ab42f946d30de00797dd9ebe
unknown <mnascimento0392@gmail.com>
2025-09-29 21:13:56 -0400

    31. Next Permutation

--- src/main/java/dev/mnascimento0392/leetCode/_31/NextPermutation.java
+++ src/main/java/dev/mnascimento0392/leetCode/_31/NextPermutation.java
@@ -1,7 +1,52 @@
 package dev.mnascimento0392.leetCode._31;
 
 class Solution {
-    public void nextPermutation(int[] nums) {
-        
-    }
+	public void nextPermutation(int[] nums) {
+		if (nums.length < 2) {
+			return;
+		}
+
+		boolean mudou = false;
+		int indexJ = -1;
+		int indexI = -1;
+		for (int i = nums.length - 1; i >= 0; i--) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (nums[j] < nums[i]) {
+					if (j > indexJ) {
+						indexJ = j;
+						indexI = i;
+					}
+					mudou = true;
+				}
+			}
+		}
+		if (mudou) {
+			trocar(nums, indexJ, indexI);
+		}
+
+		for (int j = indexJ + 1; j < nums.length; j++) {
+			for (int l = j + 1; l < nums.length; l++) {
+				if (nums[j] > nums[l]) {
+					trocar(nums, j, l);
+					mudou = true;
+					continue;
+				}
+			}
+		}
+
+		if (!mudou) {
+			for (int j = 0; j < (nums.length) / 2; j++) {
+				int aux = nums[j];
+				nums[j] = nums[nums.length - 1 - j];
+				nums[nums.length - 1 - j] = aux;
+			}
+		}
+	}
+
+	private void trocar(int[] nums, int i, int j) {
+		int aux = nums[i];
+		nums[i] = nums[j];
+		nums[j] = aux;
+	}
+
 }
--- src/test/java/dev/mnascimento0392/leetCode/_31/NextPermutationTest.java
+++ src/test/java/dev/mnascimento0392/leetCode/_31/NextPermutationTest.java
@@ -38,4 +38,45 @@ class NextPermutationTest extends TestFormatter {
 		assertArrayFormatted(input, new int[] { 1, 5, 1 });
 	}
 
+	@Test
+	void test4() {
+		input = new int[] { 1, 4, 3, 2 };
+		new Solution().nextPermutation(input);
+		assertArrayFormatted(input, new int[] { 2, 1, 3, 4 });
+	}
+
+	@Test
+	void test5() {
+		input = new int[] { 1, 2, 4, 3 };
+		new Solution().nextPermutation(input);
+		assertArrayFormatted(input, new int[] { 1, 3, 2, 4 });
+	}
+
+	@Test
+	void test6() {
+		input = new int[] { 4, 3, 2, 1 };
+		new Solution().nextPermutation(input);
+		assertArrayFormatted(input, new int[] { 1, 2, 3, 4 });
+	}
+
+	@Test
+	void test7() {
+		input = new int[] { 5, 4, 3, 2, 1 };
+		new Solution().nextPermutation(input);
+		assertArrayFormatted(input, new int[] { 1, 2, 3, 4, 5 });
+	}
+
+	@Test
+	void test8() {
+		input = new int[] { 2, 3, 1 };
+		new Solution().nextPermutation(input);
+		assertArrayFormatted(input, new int[] { 3, 1, 2 });
+	}
+
+	@Test
+	void test9() {
+		input = new int[] { 4, 2, 0, 2, 3, 2, 0 };
+		new Solution().nextPermutation(input);
+		assertArrayFormatted(input, new int[] { 4, 2, 0, 3, 0, 2, 2 });
+	}
 }
