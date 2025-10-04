ff7ec71c5e57a5e23246453bd1dd32266cd6dea7
unknown <mnascimento0392@gmail.com>
2025-10-04 15:10:05 -0400

    33. Search in Rotated Sorted Array

--- src/main/java/dev/mnascimento0392/leetCode/_33/SearchInRotatedSortedArray.java
+++ src/main/java/dev/mnascimento0392/leetCode/_33/SearchInRotatedSortedArray.java
@@ -2,7 +2,70 @@ package dev.mnascimento0392.leetCode._33;
 
 class Solution {
 	public int search(int[] nums, int target) {
-		return target;
 
+		if (nums.length == 1) {
+			if (nums[0] == target) {
+				return 0;
+			} else {
+				return -1;
+			}
+		}
+
+		if (nums.length < 4) {
+			for (int i = 0; i < nums.length; i++) {
+				if (nums[i] == target) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		int start = 0, end = nums.length - 1, indexTest = 0, leftIndex = -1;
+		while (true) {
+			if (end - start <= 1) {
+				// if()
+				leftIndex = end;
+				if (indexTest + 1 == nums.length) {
+					leftIndex = -1;
+				}
+				break;
+			}
+			indexTest = start + ((end + 1 - start) / 2);
+			if (nums[start] < nums[indexTest]) {
+				start = indexTest;
+			} else {
+				end = indexTest;
+				// break;
+			}
+		}
+
+		if (leftIndex == -1) {
+			return searchIndex(nums, 0, nums.length - 1, target);
+		}
+
+		if (target >= nums[0]) {
+			return searchIndex(nums, 0, leftIndex, target);
+		} else {
+			return searchIndex(nums, leftIndex, nums.length - 1, target);
+		}
+	}
+
+	int searchIndex(int[] nums, int start, int end, int target) {
+		int index = 0, indexTest = -1;
+		while (true) {
+			if (end - start <= 1) {
+				if (nums[start] == target)
+					return start;
+				if (nums[end] == target)
+					return end;
+				return -1;
+			}
+			indexTest = start + ((end - start) / 2);
+			if (nums[indexTest] < target) {
+				start = indexTest;
+			} else {
+				end = indexTest;
+			}
+		}
 	}
 }
--- src/test/java/dev/mnascimento0392/leetCode/_33/SearchInRotatedSortedArrayTest.java
+++ src/test/java/dev/mnascimento0392/leetCode/_33/SearchInRotatedSortedArrayTest.java
@@ -15,20 +15,83 @@ class SearchInRotatedSortedArrayTest extends TestFormatter {
 		setQuestion("33. Search in Rotated Sorted Array");
 	}
 
-		@Test
+	@Test
 	void test1() {
-		assertFormatted(new Solution().search(new int[]{ 4 ,5 ,6 ,7 ,0 ,1 ,2 }, 0), 4);
+		assertFormatted(new Solution().search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0), 4);
 	}
 
 	@Test
 	void test2() {
-		assertFormatted(new Solution().search(new int[]{ 4 ,5 ,6 ,7 ,0 ,1 ,2 }, 3), -1);
+		assertFormatted(new Solution().search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3), -1);
 	}
 
 	@Test
 	void test3() {
-		assertFormatted(new Solution().search(new int[]{ 1 }, 0), -1);
+		assertFormatted(new Solution().search(new int[] { 1 }, 0), -1);
 	}
 
+	@Test
+	void test4() {
+		assertFormatted(new Solution().search(new int[] { 0, 1, 2, 4, 5, 6, 7 }, 0), 0);
+	}
 
+	@Test
+	void test5() {
+		assertFormatted(new Solution().search(new int[] { 0, 1, 2, 4, 5, 6, 7 }, 4), 3);
+	}
+
+	@Test
+	void test6() {
+		assertFormatted(new Solution().search(new int[] { 0, 1, 2, 4, 5, 6, 7 }, 7), 6);
+	}
+
+	@Test
+	void test7() {
+		assertFormatted(new Solution().search(new int[] { 1, 3, 4, 5 }, 0), -1);
+	}
+
+	@Test
+	void test8() {
+		assertFormatted(new Solution().search(new int[] { 1, 3, 4, 5 }, 1), 0);
+	}
+
+	@Test
+	void test9() {
+		assertFormatted(new Solution().search(new int[] { 1, 3, 4, 5 }, 3), 1);
+	}
+
+	@Test
+	void test10() {
+		assertFormatted(new Solution().search(new int[] { 1, 3, 4, 5 }, 5), 3);
+	}
+
+	@Test
+	void test11() {
+		assertFormatted(new Solution().search(new int[] { 1, 3, 4, 5 }, 7), -1);
+	}
+
+	@Test
+	void test12() {
+		assertFormatted(new Solution().search(new int[] { 3, 5, 1, 2 }, 1), 2);
+	}
+
+	@Test
+	void test13() {
+		assertFormatted(new Solution().search(new int[] { 3, 5, 1, 2 }, 3), 0);
+	}
+	
+	@Test
+	void test14() {
+		assertFormatted(new Solution().search(new int[] { 7,8,1,2,3,4,5,6 }, 2), 3);
+	}
+	
+	@Test
+	void test15() {
+		assertFormatted(new Solution().search(new int[] { 5,1,2,3,4 }, 1), 1);
+	}
+	
+	@Test
+	void test16() {
+		assertFormatted(new Solution().search(new int[] {9,1,2,3,4,5,6,7,8 }, 9), 0);
+	}
 }
