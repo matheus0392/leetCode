61ed2f8e6a146d7973f6c696a8855936fc8605a0
unknown <mnascimento0392@gmail.com>
2025-10-04 17:48:18 -0400

    35. Search Insert Position

--- src/main/java/dev/mnascimento0392/leetCode/_35/SearchInsertPosition.java
+++ src/main/java/dev/mnascimento0392/leetCode/_35/SearchInsertPosition.java
@@ -2,7 +2,43 @@ package dev.mnascimento0392.leetCode._35;
 
 class Solution {
 	public int searchInsert(int[] nums, int target) {
-		return target;
+
+		int start = 0, end = nums.length - 1, index = -1;
+
+		while (start <= end) {
+
+			// index = start + (end - start + 1) / 2;
+			index = (start + end) / 2;
+
+			if (nums[index] == target) {
+				return index;
+			}
+
+			if (nums[index] < target) {
+				start = index + 1;
+			} else {
+				end = index - 1;
+			}
+		}
+		// index = (start + end ) / 2;
+
+		//if (index == nums.length - 1) {
+			if (target > nums[index]) {
+				return ++index;
+			}
+		//}
+
+		/*if (index == 0) {
+			if (target > nums[index]) {
+				return ++index;
+			}
+		}*/
+
+		/*
+		 * if (index == nums.length - 1) { index++; }
+		 */
+
+		return index;
 
 	}
 }
--- src/test/java/dev/mnascimento0392/leetCode/_35/SearchInsertPositionTest.java
+++ src/test/java/dev/mnascimento0392/leetCode/_35/SearchInsertPositionTest.java
@@ -15,20 +15,78 @@ class SearchInsertPositionTest extends TestFormatter {
 		setQuestion("35. Search Insert Position");
 	}
 
-		@Test
+	@Test
 	void test1() {
-		assertFormatted(new Solution().searchInsert(new int[]{ 1 ,3 ,5 ,6 }, 5), 2);
+		assertFormatted(new Solution().searchInsert(new int[] { 1, 3, 5, 6 }, 5), 2);
 	}
 
 	@Test
 	void test2() {
-		assertFormatted(new Solution().searchInsert(new int[]{ 1 ,3 ,5 ,6 }, 2), 1);
+		assertFormatted(new Solution().searchInsert(new int[] { 1, 3, 5, 6 }, 2), 1);
 	}
 
 	@Test
 	void test3() {
-		assertFormatted(new Solution().searchInsert(new int[]{ 1 ,3 ,5 ,6 }, 7), 4);
+		assertFormatted(new Solution().searchInsert(new int[] { 1, 3, 5, 6 }, 7), 4);
 	}
 
+	@Test
+	void test4() {
+		assertFormatted(new Solution().searchInsert(new int[] { 1 }, 0), 0);
+	}
 
+	@Test
+	void test5() {
+		assertFormatted(new Solution().searchInsert(new int[] { 1 }, 1), 0);
+	}
+
+	@Test
+	void test6() {
+		assertFormatted(new Solution().searchInsert(new int[] { 7, 8, 10 }, 7), 0);
+	}
+
+	@Test
+	void test7() {
+		assertFormatted(new Solution().searchInsert(new int[] { 7, 8, 9 }, 9), 2);
+	}
+
+	@Test
+	void test8() {
+		assertFormatted(new Solution().searchInsert(new int[] { 7, 8, 9 }, 8), 1);
+	}
+
+	@Test
+	void test9() {
+		assertFormatted(new Solution().searchInsert(new int[] { 7, 8, 9 }, 6), 0);
+	}
+
+	@Test
+	void test10() {
+		assertFormatted(new Solution().searchInsert(new int[] { 1, 3 }, 1), 0);
+	}
+
+	@Test
+	void test11() {
+		assertFormatted(new Solution().searchInsert(new int[] { 1, 3 }, 2), 1);
+	}
+
+	@Test
+	void test12() {
+		assertFormatted(new Solution().searchInsert(new int[] { 1, 3 }, 3), 1);
+	}
+
+	@Test
+	void test13() {
+		assertFormatted(new Solution().searchInsert(new int[] { 1, 2, 4, 6 }, 3), 2);
+	}
+
+	@Test
+	void test14() {
+		assertFormatted(new Solution().searchInsert(new int[] { 1, 2, 4, 6, 7 }, 3), 2);
+	}
+
+	@Test
+	void test15() {
+		assertFormatted(new Solution().searchInsert(new int[] { 1, 2, 4, 6, 7, 8 }, 3), 2);
+	}
 }
