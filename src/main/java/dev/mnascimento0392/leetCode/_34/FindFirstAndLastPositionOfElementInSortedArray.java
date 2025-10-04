21c9eb1364c3ec5455c4766d89959a49397523d0
unknown <mnascimento0392@gmail.com>
2025-10-04 16:36:00 -0400

    34. Find First and Last Position of Element in Sorted Array

--- src/main/java/dev/mnascimento0392/leetCode/_34/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/dev/mnascimento0392/leetCode/_34/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -2,7 +2,63 @@ package dev.mnascimento0392.leetCode._34;
 
 class Solution {
 	public int[] searchRange(int[] nums, int target) {
-		return nums;
+
+		if (nums.length == 0) {
+			return new int[] { -1, -1 };
+		}
+
+		if (nums.length == 1) {
+			if (nums[0] == target) {
+				return new int[] { 0, 0 };
+			} else {
+				return new int[] { -1, -1 };
+			}
+		}
+
+		int start = 0, end = nums.length - 1, index = -1;
+
+		while (true) {
+			if (end - start < 1) {
+				return new int[] { -1, -1 };
+			}
+
+			index = start + ((end + 1 - start) / 2);
+			if (nums[index] == target) {
+				break;
+			} else if (nums[index] > target) {
+				if (end == index) {
+					end--;
+					if (nums[end] == target) {
+						index = end;
+						break;
+					}
+				} else {
+					end = index;
+				}
+			} else {
+				start = index;
+			}
+		}
+
+		end = index;
+		while (true) {
+			if (nums.length > end + 1 && nums[end + 1] == target) {
+				end++;
+			} else {
+				break;
+			}
+		}
+
+		start = index;
+		while (true) {
+			if (start - 1 >= 0 && nums[start - 1] == target) {
+				start--;
+			} else {
+				break;
+			}
+		}
+
+		return new int[] { start, end };
 
 	}
 }
--- src/test/java/dev/mnascimento0392/leetCode/_34/FindFirstAndLastPositionOfElementInSortedArrayTest.java
+++ src/test/java/dev/mnascimento0392/leetCode/_34/FindFirstAndLastPositionOfElementInSortedArrayTest.java
@@ -30,4 +30,53 @@ class FindFirstAndLastPositionOfElementInSortedArrayTest extends TestFormatter {
 		assertArrayFormatted(new Solution().searchRange(new int[] {}, 0), new int[] { -1, -1 });
 	}
 
+	@Test
+	void test4() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 1 }, 0), new int[] { -1, -1 });
+	}
+
+	@Test
+	void test5() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 1 }, 1), new int[] { 0, 0 });
+	}
+
+	@Test
+	void test6() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 7, 7, 7, 8, 8, 10 }, 7), new int[] { 0, 2 });
+	}
+
+	@Test
+	void test7() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 7, 8, 8, 8, 9 }, 9), new int[] { 4, 4 });
+	}
+
+	@Test
+	void test8() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 7, 7, 8, 9, 9 }, 8), new int[] { 2, 2 });
+	}
+
+	@Test
+	void test9() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 7, 7, 7, 8, 9 }, 8), new int[] { 3, 3 });
+	}
+
+	@Test
+	void test10() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 7, 8, 9, 9, 9 }, 8), new int[] { 1, 1 });
+	}
+
+	@Test
+	void test11() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 1, 3 }, 1), new int[] { 0, 0 });
+	}
+	
+	@Test
+	void test12() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 1, 3 }, 2), new int[] {-1,-1 });
+	}
+	
+	@Test
+	void test13() {
+		assertArrayFormatted(new Solution().searchRange(new int[] { 1, 3 }, 3), new int[] { 1, 1 });
+	}
 }
