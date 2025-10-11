f4c97bc9092734140ba1ece698c511e90e8bca00
unknown <mnascimento0392@gmail.com>
2025-10-11 01:06:38 -0400

    37. Sudoku Solver

--- src/main/java/dev/mnascimento0392/leetCode/_37/SudokuSolver.java
+++ src/main/java/dev/mnascimento0392/leetCode/_37/SudokuSolver.java
@@ -1,109 +1,168 @@
 package dev.mnascimento0392.leetCode._37;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Stack;
 
 class Solution {
 
+	long count = 0;
+
 	class Input {
 		int i, j;
-		Character[][] copy;
+		char[][] copy;
+		String shrinked;
 
-		public Input(Character[][] copy, int i, int j) {
+		public Input(char[][] copy, int i, int j) {
 			this.i = i;
 			this.j = j;
 			this.copy = copy;
+			this.shrinked = stackToString(copy);
+		}
+
+		public void print() {
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					System.out.print(this.copy[i][j] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println();
 		}
 	}
 
 	public void solveSudoku(char[][] board) {
 
-		Character[][] copy = new Character[9][9];
+		List<List<List<Character>>> cube = new ArrayList<>();
+
+		for (int i = 0; i < 9; i++) {
+			cube.add(new ArrayList<>());
+			for (int j = 0; j < 9; j++) {
+				cube.get(i).add(new ArrayList<>());
+				for (int k = 1; k <= 9; k++) {
+					cube.get(i).get(j).add((char) (k + '0'));
+				}
+			}
+		}
+
+		boolean nochanges = true;
+
+		while (nochanges) {
+			nochanges = false;
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					if (board[i][j] != '.') {
+						cube.get(i).get(j).clear();
+						continue;
+					}
+
+					char[][] copy = copy(board);
+					for (int k = 0; k < 9; k++) {
+						copy[i][j] = (char) (k + '0' + 1);
+						if (!isValidSudokuInput(new Input(copy, i, j))) {
+							cube.get(i).get(j).remove(new Character(copy[i][j]));
+
+							if (cube.get(i).get(j).size() == 1) {
+								board[i][j] = cube.get(i).get(j).get(0);
+								cube.get(i).get(j).clear();
+								nochanges = true;
+							}
+						} else if (isOnlyOption(copy, i, j)) {
+							board[i][j] = copy[i][j];
+							nochanges = true;
+							break;
+						}
+					}
+
+				}
+			}
+			if (completed(board)) {
+				return;
+			}
+		}
+
+		char[][] copy = new char[9][9];
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				copy[i][j] = board[i][j];
 			}
 		}
 
+		Set<String> visited_ = new HashSet<>();
 		Stack<Input> stack = new Stack<>();
 		stack.add(new Input(copy, 0, 0));
-
-		List<Character[][]> visited_ = new ArrayList<>();
 
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				if (copy[i][j] == '.') {
+					boolean added = false;
 					for (int k = 0; k < 9; k++) {
-						Character[][] c = copy(stack.peek().copy);
+						char[][] c = copy(stack.peek().copy);
 						c[i][j] = (char) (k + '0' + 1);
-						if (!isValidSudokuInput(c, i, j)) {
+
+						if (!cube.get(i).get(j).contains(c[i][j])) {
 							continue;
 						}
 
-						if (isVisited(visited_, c)) {
+						Input input = new Input(c, i, j);
+						if (!isValidSudokuInput(input)) {
 							continue;
 						}
 
-						stack.add(new Input(c, i, j));
-							
-						if (completed(c)) {
+						if (isVisited(visited_, input.shrinked)) {
+							continue;
+						}
+
+						added = true;
+						stack.add(input);
+
+						if (!input.shrinked.contains(".")) {
 							copy = stack.peek().copy;
 							for (int ii = 0; ii < 9; ii++) {
 								for (int jj = 0; jj < 9; jj++) {
 									board[ii][jj] = copy[ii][jj];
 								}
 							}
+							System.out.println(visited_.size());
+							System.out.println(count);
 							return;
 						}
 
 					}
-				}
-				if (j == 8) {
-					Input top = stack.peek();
-					for (int f = 8; f >= 0; f--) {
-						if (top.copy[i][f] == '.') {
-							if (!stack.isEmpty()) {
-								visited_.add(stack.pop().copy);
-								Input in = stack.peek();
-								i = in.i;
-								j = in.j;
-							}
-							break;
+					if (!added) {
+						if (!stack.isEmpty()) {
+							visited_.add(stack.pop().shrinked);
+							Input in = stack.peek();
+							i = in.i;
+							j = in.j;
 						}
 					}
 				}
 			}
 		}
-		return;
+
 	}
 
-	private boolean isVisited(List<Character[][]> visited_, Character[][] c) {
-		for (Character[][] array : visited_) {
-			if (isVisited(array, c)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private boolean isVisited(Character[][] visited, Character[][] c) {
+	private String stackToString(char[][] copy) {
+		StringBuilder bs = new StringBuilder();
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				if (visited[i][j] == c[i][j]) {
-					if (i == 8 && j == 8) {
-						return true;
-					}
-				} else {
-					return false;
-				}
+				bs.append(copy[i][j]);
 			}
 		}
-		return false;
+		return bs.toString();
 	}
 
-	private Character[][] copy(Character[][] board) {
-		Character[][] copy = new Character[9][9];
+	private boolean isVisited(Set<String> visited_, String curent) {
+		count++;
+
+		return visited_.contains(curent);
+	}
+
+	private char[][] copy(char[][] board) {
+		char[][] copy = new char[9][9];
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				copy[i][j] = board[i][j];
@@ -112,7 +171,7 @@ class Solution {
 		return copy;
 	}
 
-	private boolean completed(Character[][] board) {
+	private boolean completed(char[][] board) {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				if (board[i][j] == '.') {
@@ -123,65 +182,57 @@ class Solution {
 		return true;
 	}
 
-	private boolean isValidSudokuInput(Character[][] board, int i, int j) {
+	private boolean isOnlyOption(char[][] board, int i, int j) {
+
+		char c = board[i][j];
+		board[i][j] = '.';
+
+		int boxI = i / 3, boxJ = j / 3;
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 3; x++) {
+				if (board[boxI * 3 + y][boxJ * 3 + x] == '.') {
+
+					board[boxI * 3 + y][boxJ * 3 + x] = c;
+					if (!(y == i - boxI * 3 && x == j - boxJ * 3)) {
+						if (isValidSudokuInput(new Input(board, boxI * 3 + y, boxJ * 3 + x))) {
+							board[boxI * 3 + y][boxJ * 3 + x] = '.';
+							board[i][j] = c;
+							return false;
+						}
+					}
+					board[boxI * 3 + y][boxJ * 3 + x] = '.';
+				}
+			}
+		}
+
+		board[i][j] = c;
+		return true;
+
+	}
+
+	private boolean isValidSudokuInput(Input input /* Character[][] board, int i, int j */) {
+
 		for (int x = 0; x < 9; x++) {
-			if (board[i][x] != '.') {
-				if (x != j && board[i][x] == board[i][j]) {
+			if (input.copy[input.i][x] != '.') {
+				if (x != input.j && input.copy[input.i][x] == input.copy[input.i][input.j]) {
 					return false;
 				}
 			}
-			if (board[x][j] != '.') {
-				if (x != i && board[x][j] == board[i][j]) {
+			if (input.copy[x][input.j] != '.') {
+				if (x != input.i && input.copy[x][input.j] == input.copy[input.i][input.j]) {
 					return false;
 				}
 			}
 		}
-		int boxI = i / 3, boxJ = j / 3;
+		int boxI = input.i / 3, boxJ = input.j / 3;
 		for (int x = 0; x < 3; x++) {
 			for (int y = 0; y < 3; y++) {
-				if (x != i - boxI * 3 && y != j - boxJ * 3 && board[boxI * 3 + x][boxJ * 3 + y] == board[i][j]) {
+				if (!(x == input.i - boxI * 3 && y == input.j - boxJ * 3)
+						&& input.copy[boxI * 3 + x][boxJ * 3 + y] == input.copy[input.i][input.j]) {
 					return false;
 				}
 			}
 		}
 		return true;
 	}
-
-	/*private boolean isValidSudoku(Character[][] board) {
-
-		List<Set<Character>> setRows = new ArrayList<>();
-		List<Set<Character>> setColumns = new ArrayList<>();
-		List<Set<Character>> setBoxes = new ArrayList<>();
-
-		for (int i = 0; i < 9; i++) {
-			setColumns.add(new HashSet<>());
-			setRows.add(new HashSet<>());
-			setBoxes.add(new HashSet<>());
-		}
-
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (board[i][j] != '.') {
-					if (setRows.get(i).contains(board[i][j])) {
-						return false;
-					}
-					setRows.get(i).add(board[i][j]);
-
-					if (setColumns.get(j).contains(board[i][j])) {
-						return false;
-					}
-					setColumns.get(j).add(board[i][j]);
-
-					if (setBoxes.get(j / 3 + (i / 3) * 3).contains(board[i][j])) {
-						return false;
-					}
-
-					setBoxes.get(j / 3 + (i / 3) * 3).add(board[i][j]);
-				}
-			}
-		}
-
-		return true;
-	}*/
-
 }
