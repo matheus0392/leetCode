e36d7365f29e18ef237899f0057c1bffea15535f
unknown <mnascimento0392@gmail.com>
2025-10-12 14:01:35 -0400

    39. Combination Sum

--- src/main/java/dev/mnascimento0392/leetCode/_39/CombinationSum.java
+++ src/main/java/dev/mnascimento0392/leetCode/_39/CombinationSum.java
@@ -1,10 +1,76 @@
 package dev.mnascimento0392.leetCode._39;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Solution {
-	public List<List<Integer>> combinationSum(int[] candidates, int target) {
-		return List.of();
+	int count = 0;
+	Set<String> already = new HashSet<>();
+	// Set<String> already = new HashSet<>();
 
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		int[] candidates_ = Arrays.stream(candidates).filter(c -> c <= target).toArray();
+		Arrays.sort(candidates_);
+		List<List<Integer>> resp = new ArrayList<>();
+		for (int i = 0; i < candidates_.length; i++) {
+			sum(resp, candidates_, new ArrayList<Integer>(), target, 0);
+		}
+		return resp;
+	}
+
+	private void sum(List<List<Integer>> resp, int[] candidates_, List<Integer> arrayList, int target, int sum) {
+
+		for (int i = 0; i < candidates_.length; i++) {
+			int currentSum = sum + candidates_[i];
+			if (currentSum > target) {
+				continue;
+			}
+			if (currentSum == target) {
+				List<Integer> list = new ArrayList<>();
+				list.addAll(arrayList);
+				list.add(candidates_[i]);
+				List<Integer> sorted = list.stream().sorted().toList();
+
+				/*String asString = String.valueOf(sorted.get(0));
+				if (sorted.size() > 1) {
+					asString = sorted.stream().map(a -> String.valueOf(a)).reduce((a, b) -> a + b).orElse("");
+				}
+				if (!already.contains(asString)) {
+					resp.add(sorted);
+					already.add(asString);
+				}*/
+				 if (!already(resp, sorted)) {
+		            resp.add(sorted);
+		            return;
+		         }
+				return;
+			}
+			List<Integer> list = new ArrayList<>();
+			list.addAll(arrayList);
+			list.add(candidates_[i]);
+			sum(resp, candidates_, list, target, currentSum);
+		}
+
+	}
+
+	private boolean already(List<List<Integer>> resp, List<Integer> list) {
+		for (List<Integer> each : resp) {
+			if (each.size() != list.size()) {
+				continue;
+			}
+			for (int i = 0; i < each.size(); i++) {
+				if (each.get(i) != list.get(i)) {
+					break;
+				}
+				if (i == each.size() - 1) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 }
--- src/test/java/dev/mnascimento0392/leetCode/_39/CombinationSumTest.java
+++ src/test/java/dev/mnascimento0392/leetCode/_39/CombinationSumTest.java
@@ -17,20 +17,69 @@ class CombinationSumTest extends TestFormatter {
 		setQuestion("39. Combination Sum");
 	}
 
-		@Test
+	@Test
 	void test1() {
-		assertFormatted(new Solution().combinationSum(new int[]{ 2 ,3 ,6 ,7 }, 7), List.of( 2 ,2 ,3 ,7 ));
+		assertFormatted(new Solution().combinationSum(new int[] { 2, 3, 6, 7 }, 7),
+				List.of(List.of(2, 2, 3), List.of(7)));
 	}
 
 	@Test
 	void test2() {
-		assertFormatted(new Solution().combinationSum(new int[]{ 2 ,3 ,5 }, 8), List.of( 2 ,2 ,2 ,2 ,2 ,3 ,3 ,3 ,5 ));
+		assertFormatted(new Solution().combinationSum(new int[] { 2, 3, 5 }, 8),
+				List.of(List.of(2, 2, 2, 2), List.of(2, 3, 3), List.of(3, 5)));
 	}
 
 	@Test
 	void test3() {
-		assertFormatted(new Solution().combinationSum(new int[]{ 2 }, 1), List.of(  ));
+		assertFormatted(new Solution().combinationSum(new int[] { 2 }, 1), List.of());
 	}
 
-
+	@Test
+	void test4() {
+		assertFormatted(
+				new Solution().combinationSum(new int[] { 36, 21, 2, 3, 23, 24, 38, 22, 11, 14, 15, 25, 32, 19, 35, 26,
+						31, 13, 34, 29, 12, 37, 17, 20, 39, 30, 40, 28, 27, 33 }, 35),
+				List.of(List.of(2, 2, 2, 2, 2, 2, 2, 21), List.of(2, 2, 2, 2, 3, 3, 21), List.of(2, 3, 3, 3, 3, 21),
+						List.of(2, 12, 21), List.of(3, 11, 21), List.of(14, 21),
+						List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3),
+						List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3),
+						List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 11), List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 13),
+						List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3),
+						List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 12), List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 15),
+						List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 11), List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 14),
+						List.of(2, 2, 2, 2, 2, 2, 2, 2, 2, 17), List.of(2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 13),
+						List.of(2, 2, 2, 2, 2, 2, 2, 2, 19), List.of(2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3),
+						List.of(2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 12), List.of(2, 2, 2, 2, 2, 2, 2, 3, 3, 15),
+						List.of(2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 11), List.of(2, 2, 2, 2, 2, 2, 3, 3, 3, 14),
+						List.of(2, 2, 2, 2, 2, 2, 3, 3, 17), List.of(2, 2, 2, 2, 2, 2, 3, 20),
+						List.of(2, 2, 2, 2, 2, 2, 23), List.of(2, 2, 2, 2, 2, 2, 11, 12),
+						List.of(2, 2, 2, 2, 2, 3, 3, 3, 3, 13), List.of(2, 2, 2, 2, 2, 3, 3, 19),
+						List.of(2, 2, 2, 2, 2, 3, 22), List.of(2, 2, 2, 2, 2, 3, 11, 11),
+						List.of(2, 2, 2, 2, 2, 11, 14), List.of(2, 2, 2, 2, 2, 25), List.of(2, 2, 2, 2, 2, 12, 13),
+						List.of(2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3), List.of(2, 2, 2, 2, 3, 3, 3, 3, 3, 12),
+						List.of(2, 2, 2, 2, 3, 3, 3, 3, 15), List.of(2, 2, 2, 2, 3, 24), List.of(2, 2, 2, 2, 3, 11, 13),
+						List.of(2, 2, 2, 2, 3, 12, 12), List.of(2, 2, 2, 2, 13, 14), List.of(2, 2, 2, 2, 12, 15),
+						List.of(2, 2, 2, 2, 27), List.of(2, 2, 2, 3, 3, 3, 3, 3, 3, 11),
+						List.of(2, 2, 2, 3, 3, 3, 3, 3, 14), List.of(2, 2, 2, 3, 3, 3, 3, 17),
+						List.of(2, 2, 2, 3, 3, 3, 20), List.of(2, 2, 2, 3, 3, 23), List.of(2, 2, 2, 3, 3, 11, 12),
+						List.of(2, 2, 2, 3, 11, 15), List.of(2, 2, 2, 3, 12, 14), List.of(2, 2, 2, 3, 26),
+						List.of(2, 2, 2, 3, 13, 13), List.of(2, 2, 2, 14, 15), List.of(2, 2, 2, 29),
+						List.of(2, 2, 2, 12, 17), List.of(2, 2, 3, 3, 3, 3, 3, 3, 13), List.of(2, 2, 3, 3, 3, 3, 19),
+						List.of(2, 2, 3, 3, 3, 22), List.of(2, 2, 3, 3, 3, 11, 11), List.of(2, 2, 3, 3, 11, 14),
+						List.of(2, 2, 3, 3, 25), List.of(2, 2, 3, 3, 12, 13), List.of(2, 2, 3, 11, 17),
+						List.of(2, 2, 3, 14, 14), List.of(2, 2, 3, 13, 15), List.of(2, 2, 3, 28), List.of(2, 2, 11, 20),
+						List.of(2, 2, 14, 17), List.of(2, 2, 12, 19), List.of(2, 2, 31),
+						List.of(2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3), List.of(2, 3, 3, 3, 3, 3, 3, 3, 12),
+						List.of(2, 3, 3, 3, 3, 3, 3, 15), List.of(2, 3, 3, 3, 24), List.of(2, 3, 3, 3, 11, 13),
+						List.of(2, 3, 3, 3, 12, 12), List.of(2, 3, 3, 13, 14), List.of(2, 3, 3, 12, 15),
+						List.of(2, 3, 3, 27), List.of(2, 3, 11, 19), List.of(2, 3, 15, 15), List.of(2, 3, 13, 17),
+						List.of(2, 3, 30), List.of(2, 11, 22), List.of(2, 11, 11, 11), List.of(2, 14, 19),
+						List.of(2, 13, 20), List.of(2, 33), List.of(3, 3, 3, 3, 3, 3, 3, 3, 11),
+						List.of(3, 3, 3, 3, 3, 3, 3, 14), List.of(3, 3, 3, 3, 3, 3, 17), List.of(3, 3, 3, 3, 3, 20),
+						List.of(3, 3, 3, 3, 23), List.of(3, 3, 3, 3, 11, 12), List.of(3, 3, 3, 11, 15),
+						List.of(3, 3, 3, 12, 14), List.of(3, 3, 3, 26), List.of(3, 3, 3, 13, 13), List.of(3, 3, 14, 15),
+						List.of(3, 3, 29), List.of(3, 3, 12, 17), List.of(3, 15, 17), List.of(3, 32),
+						List.of(3, 13, 19), List.of(3, 12, 20), List.of(12, 23), List.of(11, 24), List.of(13, 22),
+						List.of(11, 11, 13), List.of(11, 12, 12), List.of(15, 20), List.of(35)));
+	}
 }
