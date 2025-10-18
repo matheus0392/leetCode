143f285bee1693922d83e890efb5a993727d35fe
unknown <mnascimento0392@gmail.com>
2025-10-18 16:55:09 -0400

    43. Multiply Strings

--- src/main/java/dev/mnascimento0392/leetCode/_43/MultiplyStrings.java
+++ src/main/java/dev/mnascimento0392/leetCode/_43/MultiplyStrings.java
@@ -1,8 +1,54 @@
 package dev.mnascimento0392.leetCode._43;
 
+import java.math.BigInteger;
+import static java.math.BigInteger.valueOf;
+
+
 class Solution {
 	public String multiply(String num1, String num2) {
-		return num2;
 
+		StringBuilder sb = new StringBuilder();
+
+		BigInteger current = BigInteger.ZERO;
+
+		BigInteger base = toNum(num1);
+
+		for (int i = num2.length() - 1; i >= 0; i--) {
+			current = current.add(product(i, base, num2));
+			long lastAlg = current.mod(valueOf(10)).longValue();
+			current = current.divide(valueOf(10));
+			sb.append(lastAlg);
+		}
+
+		sb = new StringBuilder(current + sb.reverse().toString());
+
+		while (sb.charAt(0) == '0' && sb.length() > 1) {
+			sb.deleteCharAt(0);
+		}
+
+		return sb.toString();
 	}
+
+	private BigInteger product(int i, BigInteger base, String sub) {
+		return base.multiply(new BigInteger(sub.substring(i,i+1)));
+	}
+
+	/*Long toNum(String num) {
+		Long result = 0l;
+		for (int i = 0; i < num.length(); i++) {
+			result += (long) (num.charAt(i) - '0') * (long) Math.pow(10, num.length() - i - 1);
+		}
+		return result;
+	}*/
+	
+	BigInteger toNum(String num) {
+		BigInteger result = new BigInteger("0");
+		for (int i = 0; i < num.length(); i++) {
+			result= result.add( new BigInteger(num.substring(i,i+1),10).multiply(valueOf(10).pow(num.length() - i - 1)));
+		}
+		 
+		return result;
+	}
+
+
 }
--- src/test/java/dev/mnascimento0392/leetCode/_43/MultiplyStringsTest.java
+++ src/test/java/dev/mnascimento0392/leetCode/_43/MultiplyStringsTest.java
@@ -25,4 +25,34 @@ class MultiplyStringsTest extends TestFormatter {
 		assertFormatted(new Solution().multiply("123", "456"), "56088");
 	}
 
+	@Test
+	void test3() {
+		assertFormatted(new Solution().multiply("0", "0"), "0");
+	}
+
+	@Test
+	void test4() {
+		assertFormatted(new Solution().multiply("1", "0"), "0");
+	}
+
+	@Test
+	void test5() {
+		assertFormatted(new Solution().multiply("1", "1"), "1");
+	}
+
+	@Test
+	void test6() {
+		assertFormatted(new Solution().multiply("1010", "101"), "102010");
+	}
+	
+	
+	@Test
+	void test7() {
+		assertFormatted(new Solution().multiply("999999999999999999999999999999999999", "1"), "999999999999999999999999999999999999");
+	}
+	
+	@Test
+	void test8() {
+		assertFormatted(new Solution().multiply("999999999999999999999999999999999999", "999999999999999999999999999999999999"), "999999999999999999999999999999999998000000000000000000000000000000000001");
+	}
 }
